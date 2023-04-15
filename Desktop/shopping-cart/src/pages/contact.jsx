d41236a36be930aef8721cfad61f03dd4aea12d1
David Gadeken <dgadeken@gmail.com>
2023-04-15 16:02:59 -0600

    first commit

---
+++ Desktop/shopping-cart/src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./contact.css";
+
+export const Contact = () => {
+  return (
+    <div className="contact">
+        <h1 >Contact</h1>
+        <h3>Customer Support: 555-1234</h3>
+        <h3>Email: davesecommerce@noemail.com</h3>
+    </div>
+  );
+};
